62a140fd4bbb819aa30c1ce4013ebde9b28e7fd5
fernandoaguilera <fernandoaguilera@192.168.17.196>
2024-10-12 18:09:14 +0200

    Declaro este repositorio para que haga de contenedor

--- prueba git/src/principal.java
+++ prueba git/src/principal.java
@@ -1,7 +1,9 @@
 
 public class principal {
+	//utilizar este repositorio para lo que yo haga en clase
+	//para tenerlo todo en la web
 	public static void main(String[] args) {
-
+		
 		System.out.println("Nunca se terminara la clase (llora demasiado*)");
 	}
 }
